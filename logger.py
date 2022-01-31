72d8d1ffc32d01c2f0fa0255376e17543a06af49
KotonBads <kotonbads@duck.com>
2022-01-31 22:13:37 +0800

    fix getting duplicates in logs

--- logger.py
+++ logger.py
@@ -8,7 +8,5 @@ def log(log: str):
     if not os.path.exists(f'logs/{time.strftime("%Y-%m-%d")}.txt'):
         os.mknod(f'logs/{time.strftime("%Y-%m-%d")}.txt')
 
-    with open(f'logs/{time.strftime("%Y-%m-%d")}.txt', 'r+') as f:
-        a = f.readlines()
-        a.append(f'{time.strftime("%H:%M:%S")} {log}\n')
-        f.write(''.join(a))
+    with open(f'logs/{time.strftime("%Y-%m-%d")}.txt', 'a') as f:
+        f.write(f'{time.strftime("%H:%M:%S")} {log}\n')
