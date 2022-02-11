abaebc1ae7649ca2e3dcf07565a1e5484834f425
KotonBads <kotonbads@duck.com>
2022-02-11 15:47:25 +0800

    use union for func parameters

--- color.py
+++ color.py
@@ -1,23 +1,33 @@
 """
 Colors text
 """
-def red(string: str, bold: bool = False):
-    return f'\033[1;31m{string}\033[0m' if bold else f'\033[31m{string}\033[0m'
 
-def green(string: str, bold: bool = False):
-    return f'\033[1;32m{string}\033[0m' if bold else f'\033[32m{string}\033[0m'
+from typing import Union
 
-def yellow(string: str, bold: bool = False):
-    return f'\033[1;33m{string}\033[0m' if bold else f'\033[33m{string}\033[0m'
 
-def blue(string: str, bold: bool = False):
-    return f'\033[1;34m{string}\033[0m' if bold else f'\033[34m{string}\033[0m'
+def red(string: Union[str, int], bold: bool = False):
+    return f"\033[1;31m{string}\033[0m" if bold else f"\033[31m{string}\033[0m"
 
-def purple(string: str, bold: bool = False):
-    return f'\033[1;35m{string}\033[0m' if bold else f'\033[35m{string}\033[0m'
 
-def cyan(string: str, bold: bool = False):
-    return f'\033[1;36m{string}\033[0m' if bold else f'\033[36m{string}\033[0m'
+def green(string: Union[str, int], bold: bool = False):
+    return f"\033[1;32m{string}\033[0m" if bold else f"\033[32m{string}\033[0m"
 
-def white(string: str, bold: bool = False):
-    return f'\033[1;37m{string}\033[0m' if bold else f'\033[37m{string}\033[0m'
+
+def yellow(string: Union[str, int], bold: bool = False):
+    return f"\033[1;33m{string}\033[0m" if bold else f"\033[33m{string}\033[0m"
+
+
+def blue(string: Union[str, int], bold: bool = False):
+    return f"\033[1;34m{string}\033[0m" if bold else f"\033[34m{string}\033[0m"
+
+
+def purple(string: Union[str, int], bold: bool = False):
+    return f"\033[1;35m{string}\033[0m" if bold else f"\033[35m{string}\033[0m"
+
+
+def cyan(string: Union[str, int], bold: bool = False):
+    return f"\033[1;36m{string}\033[0m" if bold else f"\033[36m{string}\033[0m"
+
+
+def white(string: Union[str, int], bold: bool = False):
+    return f"\033[1;37m{string}\033[0m" if bold else f"\033[37m{string}\033[0m"
--- logger.py
+++ logger.py
@@ -23,48 +23,57 @@ except Exception as e:
 import time
 import os
 
-def mkfile(path: str = 'logs', log_format: str = '%Y-%m-%d'):
+from typing import Union
+
+
+def mkfile(path: str = "logs", log_format: str = "%Y-%m-%d"):
     """
     Creates the log directory and the log file if it doesn't exist.
     """
     if not os.path.exists(path):
         os.mkdir(path)
 
-    if not os.path.exists(f'{path}/{time.strftime(log_format)}.txt'):
-        os.mknod(f'{path}/{time.strftime(log_format)}.txt')
+    if not os.path.exists(f"{path}/{time.strftime(log_format)}.txt"):
+        os.mknod(f"{path}/{time.strftime(log_format)}.txt")
 
-def log(log: str, path: str = 'logs', log_format: str = '%Y-%m-%d'):
+
+def log(log: Union[str, Exception], path: str = "logs", log_format: str = "%Y-%m-%d"):
     """
     Use this function to show LOG information.
     """
     mkfile(path)
 
-    with open(f'{path}/{time.strftime(log_format)}.txt', 'a') as f:
+    with open(f"{path}/{time.strftime(log_format)}.txt", "a") as f:
         f.write(f'{time.strftime("LOG %H:%M:%S")} {log}\n')
 
-def warning(log: str, path: str = 'logs', log_format: str = '%Y-%m-%d'):
+
+def warning(
+    log: Union[str, Exception], path: str = "logs", log_format: str = "%Y-%m-%d"
+):
     """
     Use this function to show WARNING information.
     """
     mkfile(path)
 
-    with open(f'{path}/{time.strftime(log_format)}.txt', 'a') as f:
+    with open(f"{path}/{time.strftime(log_format)}.txt", "a") as f:
         f.write(f'{time.strftime("WARNING %H:%M:%S")} {log}\n')
 
-def error(log: str, path: str = 'logs'):
+
+def error(log: Union[str, Exception], path: str = "logs"):
     """
     Use this function to show ERROR information.
     """
     mkfile(path)
 
-    with open(f'{path}/{time.strftime("%Y-%m-%d")}.txt', 'a') as f:
+    with open(f'{path}/{time.strftime("%Y-%m-%d")}.txt', "a") as f:
         f.write(f'{time.strftime("ERROR %H:%M:%S")} {log}\n')
 
-def debug(log: str, path: str = 'logs', log_format: str = '%Y-%m-%d'):
+
+def debug(log: Union[str, Exception], path: str = "logs", log_format: str = "%Y-%m-%d"):
     """
     Use this function to show DEBUG information.
     """
     mkfile(path)
 
-    with open(f'{path}/{time.strftime(log_format)}.txt', 'a') as f:
+    with open(f"{path}/{time.strftime(log_format)}.txt", "a") as f:
         f.write(f'{time.strftime("DEBUG %H:%M:%S")} {log}\n')
